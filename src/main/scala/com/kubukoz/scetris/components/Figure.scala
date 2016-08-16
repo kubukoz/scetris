b49a7508752ab7c0d23b064c5d74317d160edf24
Jakub Kozłowski <kubukoz@gmail.com>
2016-08-17 00:33:09 +0200

    Make the rotations relative to the pivot point!

--- src/main/scala/com/kubukoz/scetris/components/Figure.scala
+++ src/main/scala/com/kubukoz/scetris/components/Figure.scala
@@ -1,25 +1,44 @@
 package com.kubukoz.scetris.components
 
+import java.awt.Color
+
+import com.kubukoz.scetris.components.Figure._
 import com.kubukoz.scetris.domain.{Offset, Rotation}
 
 import scala.swing._
 
+object Figure {
+  private def minXOffset(elems: Set[Offset]) = elems.map(_.x).min
+
+  private def minYOffset(elems: Set[Offset]) = elems.map(_.y).min
+}
+
 sealed case class Figure(leftTop: Offset, offsets: Set[Offset]) {
-  private val centerX = leftTop.x - offsets.map(_.x).min
-  private val centerY = leftTop.y - offsets.map(_.y).min
+  private val center =
+    Offset(
+      leftTop.x - minXOffset(offsets),
+      leftTop.y - minYOffset(offsets)
+    )
 
   def draw(g: Graphics2D): Unit = {
-    val blocks = offsets.map { offset =>
-      Block(centerX + offset.x, centerY + offset.y)
-    }
+    val blocks = offsets.map(_.toPosition(center))
 
     blocks foreach (_.draw(g))
   }
 
-  def rotated(rotation: Rotation) = copy(
-    offsets = offsets.map(_.rotated(rotation))
-  )
+  def rotated(rotation: Rotation) = {
+    val newOffsets = offsets.map(_.rotated(rotation))
+    val newLeftTop = Offset(
+      center.x + minXOffset(newOffsets),
+      center.y + minYOffset(newOffsets)
+    )
+
+    copy(newLeftTop, newOffsets)
+  }
 }
 
 class ZFigure(leftTop: Offset)
   extends Figure(leftTop, Set(Offset(-1, -1), Offset(0, -1), Offset(0, 0), Offset(1, 0)))
+
+class LFigure(leftTop: Offset)
+  extends Figure(leftTop, Set(Offset(-1, -1), Offset(-1, 0), Offset(0, 0), Offset(1, 0)))
--- src/main/scala/com/kubukoz/scetris/domain/Offset.scala
+++ src/main/scala/com/kubukoz/scetris/domain/Offset.scala
@@ -1,8 +1,18 @@
 package com.kubukoz.scetris.domain
 
+import com.kubukoz.scetris.components.Block
+
 case class Offset(x: Int, y: Int) {
-  def rotated(rotation: Rotation): Offset = rotation match{
+  /**
+    * Returns a new offset, rotated in the chosen direction.
+    **/
+  def rotated(rotation: Rotation): Offset = rotation match {
     case RightRotation => Offset(-y, x)
     case LeftRotation => Offset(y, -x)
   }
+
+  /**
+    * Returns an absolute position - a block
+    * */
+  def toPosition(center: Offset): Block = Block(center.x + x, center.y + y)
 }
