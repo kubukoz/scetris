cc280867cb1bd9f33e3a2df19462d4e9741aa8dd
Jakub Kozłowski <kubukoz@gmail.com>
2016-08-21 20:56:36 +0200

    Cleanup code in Figure.scala

--- src/main/scala/com/kubukoz/scetris/components/Figure.scala
+++ src/main/scala/com/kubukoz/scetris/components/Figure.scala
@@ -9,32 +9,11 @@ import com.kubukoz.scetris.meta.Config.Screen
 
 import scala.swing._
 
-object Figure {
-
-  object Singletons {
-    val Z = Figure(origin, Set(Offset(-1, -1), Offset(0, -1), origin, Offset(1, 0)), Color.CYAN)
-    val S = Z.mirror.copy(color = Color.GREEN)
-    val L = Figure(origin, Set(Offset(-1, -1), Offset(-1, 0), origin, Offset(1, 0)), Color.MAGENTA)
-    val J = L.mirror.copy(color = Color.LIGHT_GRAY)
-    val I = Figure(origin, (-1 to 2).map(Offset(_, 0)).toSet, Color.RED)
-    val O = Figure(origin, Set(Offset(-1, -1), Offset(0, -1), Offset(-1, 0), Offset(0, 0)), Color.BLUE)
-  }
-
-  val Types = {
-    import Singletons._
-    List(Z, S, L, J, I, O)
-  }
-
-  private def minXOffset(elems: Set[Offset]) = elems.map(_.x).min
-
-  private def maxXOffset(elems: Set[Offset]) = elems.map(_.x).max
-
-  private def minYOffset(elems: Set[Offset]) = elems.map(_.y).min
-
-  private def maxYOffset(elems: Set[Offset]) = elems.map(_.y).max
-}
-
 sealed case class Figure(leftTop: Offset, offsets: Set[Offset], color: Color) {
+
+  def height = (maxYOffset(offsets) - minYOffset(offsets)).abs + 1
+
+  def width = (maxXOffset(offsets) - minXOffset(offsets)).abs + 1
 
   def mirror = copy(offsets = offsets.map(off => off.copy(x = -off.x)))
 
@@ -67,10 +46,6 @@ sealed case class Figure(leftTop: Offset, offsets: Set[Offset], color: Color) {
     } && fitsScreen
   }
 
-  def height = (maxYOffset(offsets) - minYOffset(offsets)).abs + 1
-
-  def width = (maxXOffset(offsets) - minXOffset(offsets)).abs + 1
-
   def draw(g: Graphics2D): Unit = {
     val blocks = offsets.map(_.toPosition(center).toBlock)
 
@@ -87,4 +62,29 @@ sealed case class Figure(leftTop: Offset, offsets: Set[Offset], color: Color) {
 
     copy(newLeftTop, newOffsets)
   }
+}
+
+object Figure {
+
+  object Singletons {
+    val Z = Figure(origin, Set(Offset(-1, -1), Offset(0, -1), origin, Offset(1, 0)), Color.CYAN)
+    val S = Z.mirror.copy(color = Color.GREEN)
+    val L = Figure(origin, Set(Offset(-1, -1), Offset(-1, 0), origin, Offset(1, 0)), Color.MAGENTA)
+    val J = L.mirror.copy(color = Color.LIGHT_GRAY)
+    val I = Figure(origin, (-1 to 2).map(Offset(_, 0)).toSet, Color.RED)
+    val O = Figure(origin, Set(Offset(-1, -1), Offset(0, -1), Offset(-1, 0), Offset(0, 0)), Color.BLUE)
+  }
+
+  val Types = {
+    import Singletons._
+    List(Z, S, L, J, I, O)
+  }
+
+  private def minXOffset(elems: Set[Offset]) = elems.map(_.x).min
+
+  private def maxXOffset(elems: Set[Offset]) = elems.map(_.x).max
+
+  private def minYOffset(elems: Set[Offset]) = elems.map(_.y).min
+
+  private def maxYOffset(elems: Set[Offset]) = elems.map(_.y).max
 }
